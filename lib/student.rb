1e96d356cfe97690fa95da1b9d81301802b8a91e
Nathan Rathe <nprathe10@gmail.com>
2019-08-26 20:43:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
-class Student 
+class Student
   attr_accessor :name, :grade, :id
- 
+
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
@@ -8,19 +8,19 @@ class Student
   end
 
   def self.create_table
-    sql =  <<-SQL 
+    sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
+        id INTEGER PRIMARY KEY,
+        name TEXT,
         grade TEXT
         )
         SQL
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, grade) 
+      INSERT INTO songs (name, grade)
       VALUES (?, ?)
     SQL
 
